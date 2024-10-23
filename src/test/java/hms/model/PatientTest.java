54ca13668535808df49129e297966bc98d6e7f8e
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2024-10-23 22:56:49 +0800

    chore: add test classes

---
+++ src/test/java/hms/model/PatientTest.java
@@ -0,0 +1,14 @@
+package hms.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PatientTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/hms/model/UserTest.java
@@ -0,0 +1,14 @@
+package hms.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
