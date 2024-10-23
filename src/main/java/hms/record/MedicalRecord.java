38a953d81de25d45987db6a3b44ad85c626da9c5
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2024-10-23 23:18:31 +0800

    refactor: remove MedicalRecordDetails

--- src/main/java/hms/record/MedicalRecord.java
+++ src/main/java/hms/record/MedicalRecord.java
@@ -6,36 +6,44 @@ import hms.attributes.BloodType;
 import hms.attributes.Gender;
 
 public class MedicalRecord {
-
-	private final MedicalRecordDetails medicalRecordDetails;
+	private final String id;
+	private final String name;
+	private final LocalDate dateOfBirth;
+	private final Gender gender;
+	private final BloodType bloodType;
 	private String phoneNumber;
 	private String emailAddress;
 	// private AppointmentRecord appointmentRecord;
 
-	public MedicalRecord(MedicalRecordDetails medicalRecordDetails, String phoneNumber, String emailAddress) {
-		this.medicalRecordDetails = medicalRecordDetails;
+	public MedicalRecord(String id, String name, LocalDate dateOfBirth, Gender gender, BloodType bloodType,
+			String phoneNumber, String emailAddress) {
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.gender = gender;
+		this.bloodType = bloodType;
 		this.phoneNumber = phoneNumber;
 		this.emailAddress = emailAddress;
 	}
 
 	public String getId() {
-		return this.medicalRecordDetails.getId();
+		return this.id;
 	}
 
 	public String getName() {
-		return this.medicalRecordDetails.getName();
+		return this.name;
 	}
 
 	public LocalDate getDateOfBirth() {
-		return this.medicalRecordDetails.getDateOfBirth();
+		return this.dateOfBirth;
 	}
 
 	public Gender getGender() {
-		return this.medicalRecordDetails.getGender();
+		return this.gender;
 	}
 
 	public BloodType getBloodType() {
-		return this.medicalRecordDetails.getBloodType();
+		return this.bloodType;
 	}
 
 	public String getPhoneNumber() {
@@ -46,4 +54,11 @@ public class MedicalRecord {
 		return this.emailAddress;
 	}
 
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
