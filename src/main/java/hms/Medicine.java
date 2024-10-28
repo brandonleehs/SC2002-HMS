ccd36a385a3eb10d95b44dbb867e2276be89b9ce
kekgan <keagan.c.p.kong@gmail.com>
2024-10-28 20:39:49 +0800

    added quantity attribute for prescription

--- src/main/java/hms/Medicine.java
+++ src/main/java/hms/Medicine.java
@@ -2,15 +2,21 @@ package hms;
 
 public class Medicine {
 	private String name;
+	private int quantity;
 	private MedicineStatus medicineStatus;
 
-	public Medicine(String name) {
+	public Medicine(String name, int quantity) {
 		this.name = name;
+		this.quantity = quantity;
 		this.medicineStatus = MedicineStatus.PENDING;
 	}
 
 	public String getName() {
 		return this.name;
+	}
+
+	public int getQuantity(){
+		return this.quantity;
 	}
 
 	public MedicineStatus getMedicineStatus() {
