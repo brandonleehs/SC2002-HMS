7cb1dc3e08538b09f2874092dcbe72254790a2fc
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2024-10-30 01:13:10 +0800

    feat: update Patient.java and PatientTest.java

--- src/main/java/hms/model/Patient.java
+++ src/main/java/hms/model/Patient.java
@@ -1,6 +1,13 @@
 package hms.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import hms.Validation;
+import hms.appointment.Appointment;
+import hms.attributes.BloodType;
+import hms.attributes.Gender;
+import hms.record.AppointmentOutcomeRecord;
 import hms.record.MedicalRecord;
 
 public class Patient extends User {
@@ -24,6 +31,59 @@ public class Patient extends User {
 			return true;
 		}
 		return false;
+	}
+
+	public String getId() {
+		return this.medicalRecord.getId();
+	}
+
+	public String getName() {
+		return this.medicalRecord.getName();
+	}
+
+	public LocalDate getDateOfBirth() {
+		return this.medicalRecord.getDateOfBirth();
+	}
+
+	public Gender getGender() {
+		return this.medicalRecord.getGender();
+	}
+
+	public BloodType getBloodType() {
+		return this.medicalRecord.getBloodType();
+	}
+
+	public String getPhoneNumber() {
+		return this.medicalRecord.getPhoneNumber();
+	}
+
+	public String getEmailAddress() {
+		return this.medicalRecord.getEmailAddress();
+	}
+
+	public List<AppointmentOutcomeRecord> getAppointmentOutcomeRecordList() {
+		return this.medicalRecord.getAppointmentOutcomeRecordList();
+	}
+
+	public boolean scheduleAppointment(Doctor doctor, Appointment appointment) {
+		return doctor.scheduleAppointment(appointment);
+	}
+
+	public boolean rescheduleAppointment(Doctor prevDoctor, Doctor newDoctor, Appointment oldAppointment,
+			Appointment newAppointment) {
+		if (newDoctor.scheduleAppointment(newAppointment)) {
+			prevDoctor.cancelAppointment(oldAppointment);
+			return true;
+		}
+		return false;
+	}
+
+	public void cancelAppointment(Doctor doctor, Appointment appointment) {
+		doctor.cancelAppointment(appointment);
+	}
+
+	public void addAppointmentOutcomeRecord(AppointmentOutcomeRecord appointmentOutcomeRecord) {
+		this.medicalRecord.addAppointmentOutcomeRecord(appointmentOutcomeRecord);
 	}
 
 }
--- src/test/java/hms/model/PatientTest.java
+++ src/test/java/hms/model/PatientTest.java
@@ -4,17 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
+import hms.appointment.Appointment;
+import hms.appointment.AppointmentStatus;
 import hms.attributes.BloodType;
 import hms.attributes.Gender;
 import hms.record.MedicalRecord;
 
 class PatientTest {
-
 	@Test
 	public void checkIfPasswordIsChanged() {
 		MedicalRecord medicalRecord = new MedicalRecord("P1001", "Alice Brown", LocalDate.of(1980, 5, 1), Gender.FEMALE,
@@ -49,6 +51,77 @@ class PatientTest {
 		Matcher matcher = pattern.matcher(patient.getPasswordHash());
 
 		assertTrue(matcher.matches());
+	}
+
+	@Test
+	void testScheduleAppointmentIfRightTime() {
+		MedicalRecord medicalRecord = new MedicalRecord("P1001", "Alice Brown", LocalDate.of(1980, 5, 1), Gender.FEMALE,
+				BloodType.A_POS, "999", "alice.brown@example.com");
+		Patient alice = new Patient(medicalRecord, "password");
+		Doctor doctor = new Doctor("D001", "password", "John Smith", Gender.MALE, 45);
+		Appointment appointment = new Appointment(alice.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(13, 0));
+		alice.scheduleAppointment(doctor, appointment);
+		System.out.println(appointment.getAppointmentStatus());
+		assertTrue(appointment.getAppointmentStatus() == AppointmentStatus.PENDING);
+		doctor.completeAppointment(alice, appointment, "General", "Patient has a fever.");
+		assertTrue(alice.getAppointmentOutcomeRecordList().get(0) == appointment.getAppointmentOutcomeRecord());
+	}
+
+	@Test
+	void testScheduleAppointmentIfWrongTime() {
+		MedicalRecord medicalRecord = new MedicalRecord("P1001", "Alice Brown", LocalDate.of(1980, 5, 1), Gender.FEMALE,
+				BloodType.A_POS, "999", "alice.brown@example.com");
+		Patient alice = new Patient(medicalRecord, "password");
+		Doctor doctor = new Doctor("D001", "password", "John Smith", Gender.MALE, 45);
+		Appointment appointment = new Appointment(alice.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(12, 0));
+		assertFalse(alice.scheduleAppointment(doctor, appointment));
+		assertTrue(alice.getAppointmentOutcomeRecordList().isEmpty());
+	}
+
+	@Test
+	void testRescheduleAppointmentIfRightTime() {
+		MedicalRecord medicalRecord = new MedicalRecord("P1002", "Bob Stone", LocalDate.of(1975, 11, 2), Gender.MALE,
+				BloodType.B_POS, "123", "bob.stone@example.com");
+		Patient bob = new Patient(medicalRecord, "password");
+		Doctor doctor = new Doctor("D002", "password", "Emily Clarke", Gender.FEMALE, 38);
+		Appointment oldAppointment = new Appointment(bob.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(13, 0));
+		Appointment newAppointment = new Appointment(bob.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(14, 0));
+		assertTrue(bob.scheduleAppointment(doctor, oldAppointment));
+		assertTrue(bob.rescheduleAppointment(doctor, doctor, oldAppointment, newAppointment));
+		doctor.completeAppointment(bob, newAppointment, "X-Ray", "Test results negative.");
+		assertTrue(bob.getAppointmentOutcomeRecordList().get(0) == newAppointment.getAppointmentOutcomeRecord());
+	}
+
+	@Test
+	void testRescheduleAppointmentIfWrongTime() {
+		MedicalRecord medicalRecord = new MedicalRecord("P1002", "Bob Stone", LocalDate.of(1975, 11, 2), Gender.MALE,
+				BloodType.B_POS, "123", "bob.stone@example.com");
+		Patient bob = new Patient(medicalRecord, "password");
+		Doctor doctor = new Doctor("D002", "password", "Emily Clarke", Gender.FEMALE, 38);
+		Appointment oldAppointment = new Appointment(bob.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(13, 0));
+		Appointment newAppointment = new Appointment(bob.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(12, 30));
+		assertTrue(bob.scheduleAppointment(doctor, oldAppointment));
+		assertFalse(bob.rescheduleAppointment(doctor, doctor, oldAppointment, newAppointment));
+	}
+
+	@Test
+	void testCancelSchedule() {
+		MedicalRecord medicalRecord = new MedicalRecord("P1002", "Bob Stone", LocalDate.of(1975, 11, 2), Gender.MALE,
+				BloodType.B_POS, "123", "bob.stone@example.com");
+		Patient bob = new Patient(medicalRecord, "password");
+		Doctor doctor = new Doctor("D002", "password", "Emily Clarke", Gender.FEMALE, 38);
+		Appointment appointment = new Appointment(bob.getId(), doctor.getId(), LocalDate.of(2024, 10, 29),
+				LocalTime.of(13, 0));
+		bob.scheduleAppointment(doctor, appointment);
+		bob.cancelAppointment(doctor, appointment);
+		Appointment[] appointments = doctor.getSchedule().getScheduleMap().get(LocalDate.of(2024, 10, 29));
+		assertTrue(appointments[26] == null);
 	}
 
 }
