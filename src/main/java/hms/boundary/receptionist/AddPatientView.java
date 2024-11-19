967e77c141ce399617cde2c16cd0256fdafdec67
kekgan <keagan.c.p.kong@gmail.com>
2024-11-19 21:44:21 +0800

    feat: add javadoc comments

--- src/main/java/hms/boundary/receptionist/AddPatientView.java
+++ src/main/java/hms/boundary/receptionist/AddPatientView.java
@@ -12,12 +12,28 @@ import hms.exceptions.InvalidChoiceFormatException;
 import hms.exceptions.InvalidChoiceValueException;
 import hms.exceptions.InvalidDateException;
 
+/**
+ * This class represents the view responsible for handling the process of adding a new patient to the system.
+ * It collects patient details such as name, date of birth, gender, blood type, phone number, and email address.
+ * The details are then returned in a list format for use in other parts of the application.
+ */
 public class AddPatientView extends View {
+
+    /**
+     * Displays the header for the "Register New Patient" view.
+     * This method prints a bordered title to indicate the beginning of the patient registration process.
+     */
     @Override
     public void displayHeader() {
         displayBorderedText(WIDTH, "Register New Patient");
     }
 
+    /**
+     * Collects the details of the new patient, including name, date of birth, gender, blood type, phone number,
+     * and email address. The method prompts the user for input and validates the responses.
+     * 
+     * @return a list of objects containing the patient's details. If any input is invalid, returns null.
+     */
     public List<Object> getDetails() {
         List<Object> patientDetails = new ArrayList<Object>();
 
@@ -71,10 +87,19 @@ public class AddPatientView extends View {
         return patientDetails;
     }
 
+    /**
+     * Displays an error message to the user indicating that an invalid input was provided.
+     * This method is called when an exception occurs during input collection.
+     */
     private void errorMessage() {
         System.out.println("Error! Try again.");
     }
 
+    /**
+     * Displays a success message to the user indicating that the new patient has been successfully registered.
+     * 
+     * @param id the ID of the newly registered patient
+     */
     public void successMessage(String id) {
         System.out.println("New patient successfully registered.\nPatient id is " + id);
     }
--- src/main/java/hms/boundary/user/StartView.java
+++ src/main/java/hms/boundary/user/StartView.java
@@ -2,8 +2,16 @@ package hms.boundary.user;
 
 import hms.boundary.View;
 
+/**
+ * This class represents the start screen view of the application. It allows the user to either login
+ * or quit the application. The screen includes a header with the application's logo and a list of options.
+ */
 public class StartView extends View {
 
+	/**
+     * Displays the available options on the start screen. This includes the options to log in or quit the application.
+     * The options are printed with a separator line below them for visual clarity.
+     */
 	public void displayOptions() {
 		System.out.println("Please select an option:");
 		System.out.println("1. Login");
@@ -11,6 +19,10 @@ public class StartView extends View {
 		System.out.println("-".repeat(WIDTH));
 	}
 
+	/**
+     * Displays the header for the start screen. This includes the application's logo at the top of the screen.
+     * The logo is displayed by calling the static `displayLogo()` method from the parent class `View`.
+     */
 	@Override
 	public void displayHeader() {
 		View.displayLogo();
